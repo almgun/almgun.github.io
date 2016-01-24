c18cb466bf11ce38bd9e6f7faae043cca357f012
gunnar.almas <gunnar.almas@gmail.com>
2016-01-24 19:23:07 +0100

    typo

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 function login(){
-	var url = "https://www.facebook.com/v2.5/dialog/oauth?client_id=636368246436989&redirect_uri=https//almgun.github.io/callback.html&scope=email&response_type=code&state=mLM4x47pw98kQctQtyl6hgx55xuEKAFyO3haO37Z"
+	var url = "https://www.facebook.com/v2.5/dialog/oauth?client_id=636368246436989&redirect_uri=https://almgun.github.io/callback.html&scope=email&response_type=code&state=mLM4x47pw98kQctQtyl6hgx55xuEKAFyO3haO37Z"
 
 	console.log("hello");
 	location.href=url
